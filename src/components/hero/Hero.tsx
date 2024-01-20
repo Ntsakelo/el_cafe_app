ec147c1c0a4a9f77de49d3850ae64c00d537af9e
Ntsakelo Makhuvele <ntsakelo.makhuvele>
2024-01-20 13:33:44 +0200

    configured hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,11 +3,8 @@ import Effect from '../../Effect.svg';
 export const Hero = () => {
     
     return (
-        <div className="hero">
-             <div className="home_hero">
-                {/* <img className="slideImage" src={`${process.env.PUBLIC_URL}/starterCampaign.jpg`} alt="" />
-                <img src={Effect} alt="" className="effect"/> */}
-             </div> 
+        <div className="home_hero">
+
         </div>
     )
 }
