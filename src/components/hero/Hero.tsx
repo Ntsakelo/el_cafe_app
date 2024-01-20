5831ad05a587d3065ffe8a1040e02acca23f63d7
Ntsakelo Makhuvele <ntsakelo.makhuvele>
2024-01-20 13:03:42 +0200

    configured hero section classname

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
     
     return (
         <div className="hero">
-             <div className="campaign-section">
+             <div className="home_hero">
                 {/* <img className="slideImage" src={`${process.env.PUBLIC_URL}/starterCampaign.jpg`} alt="" />
                 <img src={Effect} alt="" className="effect"/> */}
              </div> 
